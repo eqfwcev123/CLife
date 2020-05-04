88372129b895ae1d33442cfbac90c9cc12b281d5
eqfwcev123 <dohyeonee95@hotmail.com>
2020-05-05 01:48:14 +0800

    User model created

---
+++ src/entity/User.ts
@@ -0,0 +1,55 @@
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+} from "typeorm";
+import { Post } from "./Post";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "varchar",
+    length: 100,
+    nullable: false,
+  })
+  username: string;
+
+  @Column({
+    type: "varchar",
+    length: 100,
+    nullable: false,
+  })
+  password: string;
+
+  @Column({
+    type: "varchar",
+    length: 150,
+    unique: true,
+    nullable: false,
+  })
+  email: string;
+
+  @Column({
+    type: "varchar",
+    length: 100,
+    nullable: false,
+  })
+  name: string;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
+
+  @OneToMany((type) => Post, (post) => post.user)
+  posts: Post[];
+}
