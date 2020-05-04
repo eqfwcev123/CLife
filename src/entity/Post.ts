c54d1f5a01123aac9bf6fc801578fcb4cf91c4af
eqfwcev123 <dohyeonee95@hotmail.com>
2020-05-05 01:48:27 +0800

    Post model created

---
+++ src/entity/Post.ts
@@ -0,0 +1,46 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { User } from "./User";
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "varchar",
+    length: 100,
+    nullable: false,
+  })
+  title: string;
+
+  @Column({
+    type: "text",
+    nullable: true,
+  })
+  information: string;
+
+  @Column({
+    type: "boolean",
+    nullable: true,
+  })
+  isImportant: boolean;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
+
+  @ManyToOne((type) => User, (user) => user.posts, {
+    nullable: false,
+    onDelete: "CASCADE",
+  })
+  user: User;
+}
