1ec89c9f463894979a72d71890842462c9e35fcd
eqfwcev123 <dohyeonee95@hotmail.com>
2020-05-06 15:53:13 +0800

    pasport for user authentication

---
+++ src/passport/index.ts
@@ -0,0 +1,23 @@
+import { getRepository } from "typeorm";
+import * as passport from "passport";
+import { User } from "../entity/User";
+import local from "./local";
+
+passport.serializeUser((user: User, done) => {
+  done(null, user.id);
+});
+
+// user.id 는 세션에 저장됨. 나중에 deserializeUser 과정에서 사용
+// serialieUser는 user객체의 어느 데이터가 세션에 저장될지 지정
+// req.session.papport.user 에 {id: XXX } 로 저장
+
+// deserializeUser의 id 는 serializeUser의 done()함수의 두번째 인자인 user.id 와 동일하다
+
+passport.deserializeUser(async (id: number, done) => {
+  try {
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne({ id: id });
+  } catch (e) {
+    console.error(e);
+  }
+});
---
+++ src/passport/local.ts
@@ -0,0 +1,42 @@
+import { getConnection, getRepository } from "typeorm";
+import * as passport from "passport";
+import { Strategy } from "passport-local";
+import * as bcrypt from "bcrypt";
+import { User } from "../entity/User";
+
+// passport.use(name, strategy)
+export default () => {
+  passport.use(
+    "local",
+    new Strategy(
+      {
+        usernameField: "user_id",
+        passwordField: "password",
+      },
+      async (username, password, done) => {
+        try {
+          let userRepository = await getConnection().getRepository(User);
+          let user = await userRepository.findOne({
+            username: username,
+          });
+          // 사용자가 존재하지 않을 경우
+          if (!user) {
+            return done(null, false, { message: "User does not exist" });
+          }
+          const result = await bcrypt.compare(password, user.password);
+          // 사용자와 비밀번호가 매칭할 경우
+          if (result) {
+            return done(null, user);
+          }
+          // 사용자는 있는데 비밀번호와 사용자계정이 맞지 않을 경우
+          return done(null, false, {
+            message: "Password and username mismatch",
+          });
+        } catch (e) {
+          done(e);
+          console.error(e);
+        }
+      }
+    )
+  );
+};
