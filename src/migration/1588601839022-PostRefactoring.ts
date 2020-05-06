e6dec1eafbbc73cf55c7bff672b3d719b7514ba2
eqfwcev123 <dohyeonee95@hotmail.com>
2020-05-06 15:52:13 +0800

    migration testing

--- src/migration/1588601839022-PostRefactoring.ts
+++ src/migration/1588601839022-PostRefactoring.ts
@@ -1,11 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class PostRefactoring1588601839022 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `ALTER TABLE post CHANGE isImportant IsImportant boolean `
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<any> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `ALTER TABLE post CHANGE IsImportant isImportant boolean `
+    );
+  }
 }
