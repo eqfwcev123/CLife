f93074ba7dda45412cace052b6f4af6917c23465
eqfwcev123 <dohyeonee95@hotmail.com>
2020-05-14 23:07:19 +0800

    minor changes for serializeUser and deserializeUser

--- src/passport/local.ts
+++ src/passport/local.ts
@@ -11,20 +11,23 @@ export default () => {
     "local",
     new Strategy(
       {
-        usernameField: "user_id",
+        usernameField: "email",
         passwordField: "password",
       },
       async (username, password, done) => {
         try {
+          console.log("2. local Strategy 호출");
           let userRepository = await getRepository(User);
           let user = await userRepository.findOne({
-            username: username,
+            email: username,
           });
+          console.dir(user);
           // 사용자가 존재하지 않을 경우
           if (!user) {
             return done(null, false, { message: "User does not exist" });
           }
           const result = await bcrypt.compare(password, user.password);
+          console.log(result);
           // 사용자와 비밀번호가 매칭할 경우
           if (result) {
             return done(null, user);
